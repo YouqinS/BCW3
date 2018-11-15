4015ee728625ed19a0d1d3cbabde4e30ed9fe330
YouqinS <33859677+YouqinS@users.noreply.github.com>
2018-11-15 14:54:29 +0200

    tasks

--- js/main.js
+++ js/main.js
@@ -1,33 +1,128 @@
+//client code: send requests
+//server code: to upload file, handle client requests, respond with responses
 
-const express = require('express'); //require the function from the express library   server?
-const app = express(); //create a constant of object express()
 
-app.get('/', (req, res) => { //call function get  //request from client, response from server
-  res.send('Hello World')
-});
 
-app.listen(3000);
 
-//const express = require('express');
-const fileUpload = require('express-fileupload');
-//const app = express();
+const express = require('express');
+const multer = require('multer');
+const bodyParser = require('body-parser');
 
-// default options
-app.use(fileUpload());
 
-app.post('/upload', function(req, res) {
-  if (Object.keys(req.files).length == 0) {
-    return res.status(400).send('No files were uploaded.');
+const app = express();
+const port = process.env.port ||3000; //const port = process.env.port || 3000;
+
+app.use(bodyParser.urlencoded({extended:false}));  //??
+app.use(bodyParser.json());
+app.use('/', express.static(__dirname)+'/public');
+
+
+//MULTER CONFIG: to get file photos to temp server storage
+const multerConfig ={
+
+  //specify diskStorage (another option is memory)
+  storage: multer.diskStorage({
+
+    //specify destination
+    destination: function(req, file, next) {
+      next(null, './public/photo');
+    },
+
+    //specify the filename to be unique
+    filename: function(req, file, next){
+      console.log(file);
+      //get the file mimetype ie 'image/jpeg' split and prefer the second value ie'jpeg'
+      const ext = file.mimetype.split('/')[1];
+      //set the file fieldname to a unique name containing the original name, current datetime and the extension.
+      next(null, file.fieldname + '-' + Date.now() + '.'+ext);
+    }
+
+  }),
+
+  // filter out and prevent non-image files.
+  fileFilter: function(req, file, next){
+    if(!file){
+      next();
+    }
+
+    // only permit image mimetypes
+    const image = file.mimetype.startsWith('image/');
+    if(image){
+      console.log('photo uploaded');
+      next(null, true);
+    }else{
+      console.log("file not supported")
+      //TODO:  A better message response to user on failure.
+      return next();
+    }
   }
 
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  let sampleFile = req.files.sampleFile;
+};
 
-  // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv('/tmp/sample.jpg', function(err) {
-    if (err)
-      return res.status(500).send(err);
 
-    res.send('File uploaded!');
-  });
+
+/* ROUTES
+**********/
+app.get('/', function(req, res){
+  res.render('index.html');
 });
+
+app.post('/upload', multer(multerConfig).single('photo'),function(req, res){
+      //Here is where I could add functions to then get the url of the new photo
+      //And relocate that to a cloud storage solution with a callback containing its new url
+      //then ideally loading that into your database solution.   Use case - user uploading an avatar...
+      res.send('Complete! Check out your public/photo-storage folder.  Please note that files not encoded with an image mimetype are rejected. <a href="index.html">try again</a>');
+    }
+
+);
+
+// RUN SERVER
+app.listen(port,function(){
+  console.log(`Server listening on port ${port}`);
+});
+
+
+
+
+
+
+
+
+// var upload = multer({ dest: 'uploads/' });
+//
+//
+// app.post('/public', upload.single('avatar'), function (req, res, next) {
+//   // req.file is the `avatar` file
+//   // req.body will hold the text fields, if there were any
+// });
+//
+// app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
+//   // req.files is array of `photos` files
+//   // req.body will contain the text fields, if there were any
+// });
+//
+// var cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
+// app.post('/cool-profile', cpUpload, function (req, res, next) {
+//   // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
+//   //
+//   // e.g.
+//   //  req.files['avatar'][0] -> File
+//   //  req.files['gallery'] -> Array
+//   //
+//   // req.body will contain the text fields, if there were any
+// });
+
+
+
+
+
+//In case you need to handle a text-only multipart form, you should use the .none() method:
+
+/*    var express = require('express')
+var app = express()
+var multer  = require('multer')
+var upload = multer()
+
+app.post('/profile', upload.none(), function (req, res, next) {
+  // req.body contains the text fields
+})*/
--- js/main_b.js
+++ js/main_b.js
@@ -13,3 +13,98 @@
 // function ends
 
 // make an event listener which calls upload function when the form is submitted
+
+
+
+
+const message = document.querySelector('#message');
+const input = document.querySelector('input[type="file"]');
+const submit = document.querySelector('input[type="submit"]');
+const data = new FormData();
+const form = document.querySelector('#uploadForm');
+
+form.addEventListener('click', upload(event));
+
+const upload = (event) =>{
+  // - prevents the form from sending
+
+  event.preventDefault();
+
+  if(input.files.length === 0){
+    //submit.disabled = true;
+  }else {
+    message.innerHTML = 'Upload in progress...';
+    data.append('fileToUpload', input.files[0]);
+
+    fetch('http://localhost:3000/tmp', {
+      method: 'POST',
+      body: data
+    }).then(response => message.innerHTML = response.body);
+  }
+};
+
+
+
+
+
+/*
+
+
+const input = document.querySelector('input[type="file"]');
+// make FormData -object
+const data = new FormData();
+// add file to FormData -object.
+// Note that 'files' is an FileList object. This means that you can upload multiple files.
+data.append('fileToUpload', input.files[0]);
+// make an object for settings
+const settings = {
+  method: 'POST',
+  // credentials: 'same-origin', // this might be needed for some servers
+  body: data
+};
+// initiate fetch. In this example the server responds with text.
+// Response could also be json. Then you would use response.json()
+fetch('https://10.114.32.120'/!*'url_to_server'*!/, settings).then((response) => {
+  return response.json();
+}).then((json) => {
+  console.log(json);
+
+ message.innerHTML=json;
+});
+*/
+
+
+
+/*
+const express = require('express'); //require the function from the express library   server?
+const app = express(); //create a constant of object express()
+
+app.get('/', (req, res) => { //call function get  //request from client, response from server
+  res.send('Hello World')
+});
+
+app.listen(3000);
+
+//const express = require('express');
+const fileUpload = require('express-fileupload');
+//const app = express();
+
+// default options
+app.use(fileUpload());
+
+app.post('/upload', function(req, res) {
+  if (Object.keys(req.files).length == 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let sampleFile = req.files.sampleFile;
+
+  // Use the mv() method to place the file somewhere on your server
+  sampleFile.mv('/tmp/sample.jpg', function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
+});*/
